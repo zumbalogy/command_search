9d59e63a69f56edf5783f7c70b29ec788120e38e
S H <you@example.com>
2018-09-03 17:06:22 -0500

    clean

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,3 @@ require('pry')
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :should }
 end
-
-# require 'clipboard'
-# def gen(x, y)
-#   out = "q('#{x}', fields).should == #{q(x, y)}"
-#   Clipboard.copy(out)
-#   pp q(x, y)
-#   out
-# end
