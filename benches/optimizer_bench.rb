1b546202398da76c965d492c1add56efd1c91ec1
S H <you@example.com>
2018-10-23 21:39:59 -0500

    optimizer specs

---
+++ benches/optimizer_bench.rb
@@ -0,0 +1,25 @@
+require('benchmark')
+
+load(__dir__ + '/../lib/command_search/lexer.rb')
+load(__dir__ + '/../lib/command_search/parser.rb')
+load(__dir__ + '/../lib/command_search/command_dealiaser.rb')
+load(__dir__ + '/../lib/command_search/optimizer.rb')
+
+$iterations = 1000
+
+Benchmark.bmbm() do |bm|
+  $bm = bm
+
+  def optimize(input)
+    title = "Optimize: #{input.inspect}"
+    lexed = CommandSearch::Lexer.lex(input)
+    parsed = CommandSearch::Parser.parse(lexed)
+    $bm.report(title) { $iterations.times { CommandSearch::Optimizer.optimize(parsed) } }
+  end
+
+  optimize('')
+  optimize(' -(a|a|b|c)')
+  optimize(' -(a|a|b|c)' * 2)
+  optimize(' -(a|a|b|c)' * 4)
+  optimize(' -(a|a|b|c)' * 8)
+end
