db98d8cd5334f5d0f109a66695e3be8cba265866
S H <you@example.com>
2018-10-28 10:02:07 -0500

    bencing decomposing unaliasable commands

--- benches/command_dealiaser_bench.rb
+++ benches/command_dealiaser_bench.rb
@@ -10,10 +10,14 @@ Benchmark.bmbm() do |bm|
   $bm = bm
 
   def dealias(input, command_fields)
-    title = "Dealias: #{input.inspect}"
     lexed = CommandSearch::Lexer.lex(input)
     parsed = CommandSearch::Parser.parse(lexed)
-    $bm.report(title) { $iterations.times { CommandSearch::CommandDealiaser.dealias(parsed, command_fields) } }
+    $bm.report("Decompose: #{input.inspect}") { $iterations.times {
+      CommandSearch::CommandDealiaser.decompose_unaliasable(parsed, command_fields)
+    } }
+    $bm.report('Dealias') { $iterations.times {
+      CommandSearch::CommandDealiaser.dealias(parsed, command_fields)
+    } }
   end
 
   dealias('', {})
@@ -23,8 +27,7 @@ Benchmark.bmbm() do |bm|
   dealias('foo:bar ' * 10, { foo: :abc, abc: :xyz, xyz: String })
   dealias('fo:bar ' * 10, { foo: :abc, abc: :xyz, xyz: String })
   dealias('fo:bar ' * 10, { })
+  dealias('fo:bar a:a b:b c:c ' * 1, { })
+  dealias('fo:bar a:a b:b c:c ' * 2, { })
+  dealias('fo:bar a:a b:b c:c ' * 4, { })
 end
-
-
-# TODO: bench decomposing.
-# cleaned = CommandDealiaser.decompose_unaliasable(dealiased, command_fields)
