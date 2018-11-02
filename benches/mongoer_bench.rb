776be2d52d333eae976105f207588d0ff17b04a7
S H <you@example.com>
2018-11-01 21:29:22 -0500

    benchnarks with totals and the breakdown in mongoer benches

--- benches/mongoer_bench.rb
+++ benches/mongoer_bench.rb
@@ -1,5 +1,7 @@
 require('benchmark')
 
+include Benchmark
+
 load(__dir__ + '/../lib/command_search/lexer.rb')
 load(__dir__ + '/../lib/command_search/parser.rb')
 load(__dir__ + '/../lib/command_search/command_dealiaser.rb')
@@ -10,36 +12,36 @@ class Boolean; end
 
 $iterations = 1000
 
-Benchmark.bmbm() do |bm|
-  $bm = bm
-
-  def mongo(input, fields, command_fields)
-    $bm.report("Lex: #{input.inspect}") { $iterations.times {
+def mongo(input, fields, command_fields)
+  Benchmark.benchmark(CAPTION, 60, FORMAT, 'Total:') do |bm|
+    l = bm.report("Lex: #{input.inspect}") { $iterations.times {
       $lexed = CommandSearch::Lexer.lex(input)
     }}
-    $bm.report('P') { $iterations.times {
+    p = bm.report('P') { $iterations.times {
       $parsed = CommandSearch::Parser.parse($lexed)
     }}
-    $bm.report('D') { $iterations.times {
+    d = bm.report('D') { $iterations.times {
       $dealiased = CommandSearch::CommandDealiaser.dealias($parsed, command_fields)
     }}
-    $bm.report('U') { $iterations.times {
+    u = bm.report('U') { $iterations.times {
       $cleaned = CommandSearch::CommandDealiaser.decompose_unaliasable($dealiased, command_fields)
     }}
-    $bm.report('O') { $iterations.times {
+    o = bm.report('O') { $iterations.times {
       $opted = CommandSearch::Optimizer.optimize($cleaned)
     }}
-    $bm.report('M') { $iterations.times {
+    m = bm.report('M') { $iterations.times {
       CommandSearch::Mongoer.build_query($opted, fields, command_fields)
     }}
+    [l + p + d + u + o + m]
   end
-
-  fields = [:title, :description, :tags]
-  command_fields = { has_child_id: Boolean, title: String, name: :title }
-  mongo('', [], {})
-  mongo('', fields, command_fields)
-  mongo('foo bar', fields, command_fields)
-  mongo('-(a)|"b"', fields, command_fields)
-  mongo('name:foo tile -(foo bar)', fields, command_fields)
-  mongo('name:foo tile -(foo bar)|"hello world" foo>1.2', fields, command_fields)
 end
+
+fields = [:title, :description, :tags]
+command_fields = { has_child_id: Boolean, title: String, name: :title }
+
+mongo('', [], {})
+mongo('', fields, command_fields)
+mongo('foo bar', fields, command_fields)
+mongo('-(a)|"b"', fields, command_fields)
+mongo('name:foo tile -(foo bar)', fields, command_fields)
+mongo('name:foo tile -(foo bar)|"hello world" foo>1.2', fields, command_fields)
