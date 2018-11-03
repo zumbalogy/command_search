569ffe40746a010f6bc122ed228b122f798f57d9
S H <you@example.com>
2018-11-02 20:27:13 -0500

    apostrophes

--- lib/command_search/lexer.rb
+++ lib/command_search/lexer.rb
@@ -29,7 +29,7 @@ module CommandSearch
           type = :paren
         when /^[<>]=?/
           type = :compare
-        when /^\d*[^\d\s:)][^\s"'|:<>)(]*/
+        when /^[^\s:"|<>()]+/
           type = :str
         when /^./
           type = :str
