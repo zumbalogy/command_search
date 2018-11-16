9d0fffe798e7c7ccaf40fc44685351af43c6bcb7
S H <you@example.com>
2018-11-15 23:09:36 -0600

    notes

--- lib/command_search/command_dealiaser.rb
+++ lib/command_search/command_dealiaser.rb
@@ -1,29 +1,28 @@
 module CommandSearch
   module CommandDealiaser
-    module_function
 
-    def dealias_key(key, aliases)
+    def self.dealias_key(key, aliases)
       key = aliases[key.to_sym] while aliases[key.to_sym].is_a?(Symbol)
       key.to_s
     end
 
-    def dealias_values((key_node, search_node), aliases)
+    def self.dealias_values((key_node, search_node), aliases)
       new_key = dealias_key(key_node[:value], aliases)
       key_node[:value] = new_key
       [key_node, search_node]
     end
 
-    def unnest_unaliased(node, aliases)
+    def self.unnest_unaliased(node, aliases)
       type = node[:nest_type]
       values = node[:value].map { |x| x[:value].to_sym }
-      return node if type == :colon && aliases[values.first]
-      return node if type == :compare && (values & aliases.keys).any?
+      return node if type == :colon && aliases[values.first] # look into this lines test coverage and all
+      return node if type == :compare && (values & aliases.keys).any? # look into this lines test coverage and all
       str_values = values.join(node[:nest_op])
       { type: :str, value: str_values }
     end
 
-    def dealias(ast, aliases)
-      ast.flat_map do |x|
+    def self.dealias(ast, aliases)
+      ast.flat_map do |x| # look into map!
         next x unless x[:nest_type]
         x[:value] = dealias(x[:value], aliases)
         next x unless [:colon, :compare].include?(x[:nest_type])
@@ -32,8 +31,8 @@ module CommandSearch
       end
     end
 
-    def decompose_unaliasable(ast, aliases)
-      ast.flat_map do |x|
+    def self.decompose_unaliasable(ast, aliases)
+      ast.flat_map do |x| # look into map!
         next x unless x[:nest_type]
         x[:value] = decompose_unaliasable(x[:value], aliases)
         next x unless [:colon, :compare].include?(x[:nest_type])
