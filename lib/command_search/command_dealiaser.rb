bdc22b20d64c6b4c7350f67f36dcdba6aeac95ab
S H <you@example.com>
2018-11-16 21:43:29 -0600

    clean and benchmark with lots of aliased commands

--- lib/command_search/command_dealiaser.rb
+++ lib/command_search/command_dealiaser.rb
@@ -15,14 +15,14 @@ module CommandSearch
     def self.unnest_unaliased(node, aliases)
       type = node[:nest_type]
       values = node[:value].map { |x| x[:value].to_sym }
-      return node if type == :colon && aliases[values.first] # look into this lines test coverage and all
-      return node if type == :compare && (values & aliases.keys).any? # look into this lines test coverage and all
+      return node if type == :colon && aliases[values.first]
+      return node if type == :compare && (values & aliases.keys).any?
       str_values = values.join(node[:nest_op])
       { type: :str, value: str_values }
     end
 
     def self.dealias(ast, aliases)
-      ast.map! do |x| # look into map!
+      ast.map! do |x|
         next x unless x[:nest_type]
         dealias(x[:value], aliases)
         next x unless [:colon, :compare].include?(x[:nest_type])
@@ -32,7 +32,7 @@ module CommandSearch
     end
 
     def self.decompose_unaliasable(ast, aliases)
-      ast.map! do |x| # look into map!
+      ast.map! do |x|
         next x unless x[:nest_type]
         decompose_unaliasable(x[:value], aliases)
         next x unless [:colon, :compare].include?(x[:nest_type])
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :should }
 
   # # if ENV['MUTANT']
-  config.around(:each) do |example|
-    Timeout.timeout(5, &example)
-  end
+  # config.around(:each) do |example|
+  #   Timeout.timeout(5, &example)
+  # end
 end
