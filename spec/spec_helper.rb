658e3f1c9210e02ddb4afce88f483843f9f6700d
S H <you@example.com>
2018-11-17 20:43:15 -0600

    clean

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ load(__dir__ + '/../lib/command_search.rb')
 require('rspec')
 require('pry')
 
-require('timeout')
-
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :should }
 end
