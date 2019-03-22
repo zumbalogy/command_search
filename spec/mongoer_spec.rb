875dd3902050063037bd806b14f75505815175d4
zumbalogy <intrapostal@gmail.com>
2019-03-21 22:16:20 -0500

    updated spec

--- spec/mongoer_spec.rb
+++ spec/mongoer_spec.rb
@@ -42,7 +42,7 @@ describe CommandSearch::Mongoer do
     def q2(s); q(s, ['f1'], { str1: String }); end
     q2('"a b"').should == { 'f1' => /\ba\ b\b/ }
     q2("str1:'a-b'").should == { 'str1' => /\ba\-b\b/ }
-    q2("str1:'a+'").should == { 'str1' => /(?<=^|[^:+\w])a\+(?=$|[^:+\w])/ }
+    q2("str1:'a+'").should == { 'str1' => /(^|\s|[^:+\w])a\+($|\s|[^:+\w])/ }
   end
 
   it 'should handle numeric type general fields' do
