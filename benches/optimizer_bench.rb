75259d34507245c13b3a19d8b1ad699dab72c47c
zumbalogy <intrapostal@gmail.com>
2019-09-13 01:20:23 -0500

    benchmark-ips for optimizer bench

--- benches/optimizer_bench.rb
+++ benches/optimizer_bench.rb
@@ -1,22 +1,28 @@
-require('benchmark')
+require('benchmark/ips')
 
 load(__dir__ + '/../lib/command_search.rb')
 
-$iterations = 1000
-
-Benchmark.bmbm() do |bm|
+Benchmark.ips() do |bm|
   $bm = bm
 
   def optimize(input)
-    title = "Optimize: #{input.inspect}"
+    title = "Optimize #{input.length}: #{input.inspect[0..24]}"
     lexed = CommandSearch::Lexer.lex(input)
     parsed = CommandSearch::Parser.parse!(lexed)
-    $bm.report(title) { $iterations.times { CommandSearch::Optimizer.optimize(parsed) } }
+    $bm.report(title) { CommandSearch::Optimizer.optimize(parsed) }
   end
 
   optimize('')
-  optimize(' -(a|a|b|c)')
-  optimize(' -(a|a|b|c)' * 2)
-  optimize(' -(a|a|b|c)' * 4)
-  optimize(' -(a|a|b|c)' * 8)
+  optimize('a|b|(a|b|c)|')
+  optimize('a|a a|b|(a|b|c)|')
+  optimize('a (b c) a|a a|b|(a|b|c)|')
+  optimize('(((a))) (a (a ((a)))) a (b c) a|a a|b|(a|b|c)|')
+  optimize('a lemon a -() a b (a b (a b)) -((-())) (((a))) (a (a ((a)))) a (b c) a|a a|b|(a|b|c)|')
+  optimize('a lemon a -() a b (a b (a b)) -((-())) (((a))) (a (a ((a)))) a (b c) a|a a|b|(a|b|c)|' * 10)
+  optimize('a lemon a -() a b (a b (a b)) -((-())) (((a))) (a (a ((a)))) a (b c) a|a a|b|(a|b|c)|' * 100)
+  optimize('a lemon a -() a b (a b (a b)) -((-())) (((a))) (a (a ((a)))) a (b c) a|a a|b|(a|b|c)|' * 500)
 end
+
+
+# TODO: test some really long strings into the system (like 50k characters) to see when it breaks and all)
+# and then maybe cap the inputs, and put a note in the readme saying theres a cap but users might want to cap it further
