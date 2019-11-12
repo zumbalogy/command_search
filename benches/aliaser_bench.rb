9ceead4b76a9cba9150df09d1d7dd724a580c7b8
zumbalogy <intrapostal@gmail.com>
2019-11-12 17:46:49 -0500

    whitespace

--- benches/aliaser_bench.rb
+++ benches/aliaser_bench.rb
@@ -20,10 +20,18 @@ Benchmark.ips do |bm|
   bench('foo bar', { 'foo' => 'bar', 'bar' => 'abc' })
   bench('foo bar', { 'foo' => 'bar', 'a' => 'b', 'b' => 'c', 'c' => 'd' })
 
-  bench('foo fn', { 'fn' => -> (match) { match * 2 } })
-  bench('foo fn fn', { 'fn' => -> (match) { match * 2 } })
-  bench('foo fn "fn"', { 'fn' => -> (match) { match * 2 } })
-  bench('foo fn "fn"' * 100, { 'fn' => -> (match) { match * 2 } })
-  bench('foo fn fn1', { 'fn' => -> (match) { match * 2 } })
-  bench('foo fn fn1 fn2', { 'fn' => -> (match) { match * 2 }, 'fn1' => -> (m) { m + 'ddsdf' }, 'fn2' => -> (m) { m[0] } })
+  bench('foo fn',            { 'fn' => -> (m) { m * 2 } })
+  bench('foo fn fn',         { 'fn' => -> (m) { m * 2 } })
+  bench('foo fn "fn"',       { 'fn' => -> (m) { m * 2 } })
+  bench('foo fn "fn"' * 100, { 'fn' => -> (m) { m * 2 } })
+  bench('foo fn fn1',        { 'fn' => -> (m) { m * 2 } })
+
+  bench(
+    'foo fn fn1 fn2',
+    {
+      'fn' => -> (m) { m * 2 },
+      'fn1' => -> (m) { m + 'ddsdf' },
+      'fn2' => -> (m) { m[0] }
+    }
+  )
 end
