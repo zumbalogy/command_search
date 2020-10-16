00e2e367baabe8d13e8de9217d157903e426ea99
_ <foo@bar.baz>
2020-10-15 23:39:30 -0700

    debug

--- spec/integration/mysql_spec.rb
+++ spec/integration/mysql_spec.rb
@@ -12,10 +12,7 @@ module MySQL_Spec
   DB.query("DROP DATABASE IF EXISTS #{db_name}")
   DB.query("CREATE DATABASE #{db_name}")
   DB.select_db(db_name)
-  puts DB.query('SHOW VARIABLES WHERE Variable_name = "version"')
-  puts DB.query('SHOW VARIABLES WHERE Variable_name = "version"')
-  puts DB.query('SHOW VARIABLES WHERE Variable_name = "version"')
-  puts DB.query('SHOW VARIABLES WHERE Variable_name = "version"')
+  puts DB.query('SHOW VARIABLES').to_a
   DB_VERSION = DB.query('SHOW VARIABLES WHERE Variable_name = "version"').first['Value']
 
   hat_schema = "
