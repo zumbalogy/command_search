1419a920956e5694e04b9e862046b19aa0d2ad93
_ <foo@bar.baz>
2020-10-17 16:38:12 -0700

    wrap db setups

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,24 +5,27 @@ require('coderay')
 require('pry-byebug')
 require('binding_of_caller')
 
-require('mongoid')
+begin
+  require('mongoid')
+  require('active_record')
+  require('sqlite3')
+  require('mysql2')
+  require('pg')
 
-require('active_record')
-require('sqlite3')
-require('mysql2')
-require('pg')
-
-unless ENV['MONGOID_CONFIGURED']
-  ENV['MONGOID_CONFIGURED'] = 'true'
-  Mongoid.configure do |config|
-    host = 'localhost'
-    port = ENV.fetch("MONGODB_PORT") { '27017' }
-    config.clients.default = {
-      hosts: ["#{host}:#{port}"],
-      database: 'mongoid_test'
-    }
+  unless ENV['MONGOID_CONFIGURED']
+    ENV['MONGOID_CONFIGURED'] = 'true'
+    Mongoid.configure do |config|
+      host = 'localhost'
+      port = ENV.fetch("MONGODB_PORT") { '27017' }
+      config.clients.default = {
+        hosts: ["#{host}:#{port}"],
+        database: 'mongoid_test'
+      }
+    end
+    Mongo::Logger.logger.level = Logger::FATAL
   end
-  Mongo::Logger.logger.level = Logger::FATAL
+rescue
+  puts "DB Integrations Failed"
 end
 
 def pp(*inputs)
