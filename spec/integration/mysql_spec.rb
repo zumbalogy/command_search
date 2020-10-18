1570f8bbf619b611e4106f5ee38565471db44f67
_ <foo@bar.baz>
2020-10-18 16:35:46 -0700

    better command_serach_specs

--- spec/integration/mysql_spec.rb
+++ spec/integration/mysql_spec.rb
@@ -2,10 +2,10 @@ load(__dir__ + '/integration_helper.rb')
 
 module MySQL_Spec
 
-  db_name = 'command_search_db_test'
+  db_name = 'isolated_command_search_db_test'
   DB = Mysql2::Client.new(
     host: '127.0.0.1',
-    port:  ENV.fetch("MYSQL_PORT") { '3306' },
+    port:  '3306',
     username: 'root',
   )
 
