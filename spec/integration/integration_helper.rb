9ba5099a33fa196f31e2f337aade330557b362ac
_ <foo@bar.baz>
2020-10-19 00:14:37 -0700

    diff helper

--- spec/integration/integration_helper.rb
+++ spec/integration/integration_helper.rb
@@ -1,3 +1,9 @@
+$CONFIGURED ||= false
+return if $CONFIGURED
+$CONFIGURED = true
+
+puts "hello"
+
 load(__dir__ + '/../spec_helper.rb')
 
 require('mongoid')
@@ -6,25 +12,20 @@ require('sqlite3')
 require('mysql2')
 require('pg')
 
-unless ENV['CONFIGURED']
-
-  ENV['CONFIGURED'] = 'true'
-  Mongoid.configure do |config|
-    config.clients.default = {
-      hosts: ['localhost:27017'],
-      database: 'mongoid_test'
-    }
-  end
-  Mongo::Logger.logger.level = Logger::FATAL
-
-  mysql_db_name = 'command_search_db_test'
-  DB = Mysql2::Client.new(
-    host: '127.0.0.1',
-    port:  '3306',
-    username: 'root',
-  )
-  DB.select_db(mysql_db_name)
-  DB.query("DROP DATABASE IF EXISTS #{mysql_db_name}")
-  DB.query("CREATE DATABASE #{mysql_db_name}")
-
+Mongoid.configure do |config|
+  config.clients.default = {
+    hosts: ['localhost:27017'],
+    database: 'mongoid_test'
+  }
 end
+Mongo::Logger.logger.level = Logger::FATAL
+
+mysql_db_name = 'command_search_db_test'
+DB = Mysql2::Client.new(
+  host: '127.0.0.1',
+  port:  '3306',
+  username: 'root',
+)
+DB.select_db(mysql_db_name)
+DB.query("DROP DATABASE IF EXISTS #{mysql_db_name}")
+DB.query("CREATE DATABASE #{mysql_db_name}")
